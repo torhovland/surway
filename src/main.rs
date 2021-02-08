1f2a7b097bf4eb005adb3d8fc964b4ba36624bff
Tor Hovland <tor.hovland@bekk.no>
2021-02-08 21:13:46 +0100

    Seed counter sample.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use seed::{prelude::*, *};
+
+type Model = i32;
+
+#[derive(Copy, Clone)]
+enum Msg {
+    Increment,
+}
+
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::default()
+}
+
+fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Increment => *model += 1,
+    }
+}
+
+fn view(model: &Model) -> Node<Msg> {
+    div![
+        "This is a counter: ",
+        button![model, ev(Ev::Click, |_| Msg::Increment),],
+    ]
+}
+
 fn main() {
-    web_sys::console::log_1(&"Hello, world!".into());
+    App::start("app", init, update, view);
 }
