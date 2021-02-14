d9d2f4367fab4358e57ef7e59b0471f7d885cde7
Tor Hovland <tor.hovland@bekk.no>
2021-02-14 15:36:36 +0100

    Adding a Leaflet map.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use seed::{prelude::*, *};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct MapConfig {
+    pub center: [f32; 2],
+    pub zoom: f32,
+}
 
 type Model = i32;
 
@@ -8,6 +15,12 @@ enum Msg {
 }
 
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    let map_config = MapConfig { center: [63.5, 10.09], zoom: 5.0 };
+    let js_map_config = JsValue::from_serde(&map_config).unwrap();
+    let layer = leaflet::TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", &JsValue::NULL);
+    let map = leaflet::Map::new("map", &js_map_config);
+    layer.addTo(&map);
+
     Model::default()
 }
 
