b2cbdcf4649ba1f008825320b5ddc2a2e3140487
Tor Hovland <tor.hovland@bekk.no>
2021-02-20 10:46:00 +0100

    Error message.

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,7 @@ fn init_map(_: RenderInfo) {
     web_sys::console::log_1(&"init_map".into());
 
     let map_config = MapConfig { center: [63.5, 10.09], zoom: 5.0 };
-    let js_map_config = JsValue::from_serde(&map_config).unwrap();
+    let js_map_config = JsValue::from_serde(&map_config).expect("Unable to serialize map config");
     let layer = leaflet::TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", &JsValue::NULL);
     let map = leaflet::Map::new("map", &js_map_config);
     layer.addTo(&map);
