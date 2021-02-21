ba269cc32189e2b7e8bb2f5fd8b57ab2e389af1a
Tor Hovland <tor.hovland@bekk.no>
2021-02-21 12:59:12 +0100

    More Leaflet vector layers.

--- src/map.rs
+++ src/map.rs
@@ -1,16 +1,40 @@
+use leaflet::{Circle, LatLng, LatLngBounds, Map, Polygon, Polyline, Rectangle, TileLayer};
 use seed::{prelude::*};
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct MapConfig {
-    pub center: [f32; 2],
-    pub zoom: f32,
+struct CircleOptions {
+    radius: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct PolylineOptions {
+    color: String,
 }
 
 pub fn init(_: RenderInfo) {
-    let map_config = MapConfig { center: [63.5, 10.09], zoom: 5.0 };
-    let js_map_config = JsValue::from_serde(&map_config).expect("Unable to serialize map config");
-    let layer = leaflet::TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", &JsValue::NULL);
-    let map = leaflet::Map::new("map", &js_map_config);
-    layer.addTo(&map);
+    let map = Map::new("map", &JsValue::NULL);
+    map.setView(&LatLng::new(63.5, 10.5), 5.0);
+
+    TileLayer::new("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", &JsValue::NULL).addTo(&map);
+
+    Polyline::new_with_options(
+        [LatLng::new(63.25, 11.25), LatLng::new(63.75, 11.75), LatLng::new(63.5, 12.0)]
+            .iter().map(JsValue::from).collect(),
+        &JsValue::from_serde(&PolylineOptions { color: "red".into() }).expect("Unable to serialize polyline options")
+    ).addTo(&map);
+
+    Polygon::new(
+        [LatLng::new(63.25, 12.25), LatLng::new(63.75, 12.75), LatLng::new(63.5, 13.0)]
+            .iter().map(JsValue::from).collect()
+    ).addTo(&map);
+
+    Rectangle::new(&LatLngBounds::new(&LatLng::new(63.25, 10.25), &LatLng::new(63.75, 10.75))).addTo(&map);
+
+    Circle::new(&LatLng::new(63.25, 13.25)).addTo(&map);
+
+    Circle::new_with_options(
+        &LatLng::new(63.25, 13.35), 
+        &JsValue::from_serde(&CircleOptions { radius: 4000.0 }).expect("Unable to serialize circle options")
+    ).addTo(&map);
 }
