a91b7beb7aac48c2aa0da6682f6d5f7c07582487
Tor Hovland <tor.hovland@bekk.no>
2021-05-28 10:36:36 +0200

    Wake-lock, work in progress.

--- src/map.rs
+++ src/map.rs
@@ -3,11 +3,14 @@ use crate::{
     osm::OsmNode,
     Model,
 };
+use gloo_events::EventListener;
+use js_sys::{Array, Function};
 use leaflet::{
-    Circle, LatLng, LatLngBounds, LayerGroup, Map, Marker, Polyline, Rectangle, TileLayer,
+    Circle, Control, LatLng, LatLngBounds, LayerGroup, Map, Marker, Polyline, Rectangle, TileLayer,
 };
-use seed::prelude::*;
+use seed::{prelude::*, window};
 use serde::{Deserialize, Serialize};
+use web_sys_wake_lock::{console, Element, HtmlAnchorElement};
 
 #[derive(Serialize, Deserialize)]
 #[allow(non_snake_case)]
@@ -25,6 +28,16 @@ struct CircleOptions {
 #[derive(Serialize, Deserialize)]
 struct MarkerOptions {}
 
+#[derive(Serialize, Deserialize)]
+struct ControlOptions {
+    position: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ControlProps {
+    options: ControlOptions,
+}
+
 pub fn init() -> (Map, LayerGroup, LayerGroup, LayerGroup) {
     let map = Map::new("map", &JsValue::NULL);
 
@@ -36,6 +49,8 @@ pub fn init() -> (Map, LayerGroup, LayerGroup, LayerGroup) {
 
     let notes_layer_group = LayerGroup::new();
     notes_layer_group.addTo(&map);
+
+    add_wake_lock_control(&map);
 
     TileLayer::new(
         "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -174,6 +189,77 @@ fn bbox(position: &Coord, radius: f64) -> LatLngBounds {
         &LatLng::new(south.lat, west.lon),
         &LatLng::new(north.lat, east.lon),
     )
+}
+
+fn add_wake_lock_control(map: &Map) {
+    let props = JsValue::from_serde(&ControlProps {
+        options: ControlOptions {
+            position: "topleft".into(),
+        },
+    })
+    .expect("Unable to serialize control props");
+
+    // This callback must return a HTML div representing the control button.
+    let on_add = || {
+        let document = window().document().expect("Unable to get browser document");
+
+        let container = document
+            .create_element("div")
+            .expect("Unable to create div");
+
+        container.set_class_name("leaflet-bar");
+
+        let link = document
+            .create_element("a")
+            .expect("Unable to create link")
+            .dyn_into::<web_sys_wake_lock::HtmlAnchorElement>()
+            .expect("Unable to cast to HtmlAnchorElement");
+
+        link.set_href("#");
+        link.set_inner_html("⬤");
+        link.set_title("Create a new foobar.");
+
+        let on_click = EventListener::new(&link, "click", async |_| {
+            let wake_lock = web_sys_wake_lock::window()
+                .expect("Unable to get browser window.")
+                .navigator()
+                .wake_lock();
+
+            let promise = wake_lock.request(web_sys_wake_lock::WakeLockType::Screen);
+            let result = wasm_bindgen_futures::JsFuture::from(promise)
+                .await
+                .expect("Unable to convert promise.");
+
+            console::log_1(&"Control button click.".into());
+            console::log_1(result.into());
+        });
+
+        on_click.forget();
+
+        container
+            .append_child(&link)
+            .expect("Unable to add child element");
+
+        container
+    };
+
+    let on_add_closure = Closure::wrap(Box::new(on_add) as Box<dyn FnMut() -> Element>);
+
+    js_sys::Reflect::set(&props, &JsValue::from("onAdd"), on_add_closure.as_ref())
+        .expect("Unable to set onAdd()");
+
+    on_add_closure.forget();
+
+    let control_class = Control::extend(&props)
+        .dyn_into::<Function>()
+        .expect("Unable to cast to Function");
+
+    let control_button: Control = JsCast::unchecked_into(
+        js_sys::Reflect::construct(&control_class, &Array::new())
+            .expect("Unable to run constructor"),
+    );
+
+    control_button.addTo(&map);
 }
 
 impl From<&Coord> for LatLng {
