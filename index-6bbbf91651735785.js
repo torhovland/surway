75613ede57773e981a39a7ff150e5c4c0ee71077
torhovland <torhovland@users.noreply.github.com>
2021-11-22 14:59:53 +0000

    deploy: f653fb0e4b9920ac2a0b0444401b057bdbffe74e

--- index-6bbbf91651735785.js
+++ index-6bbbf91651735785.js
@@ -304,7 +304,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d571cb81e0e2923f_bg.wasm', import.meta.url);
+        input = new URL('index-6bbbf91651735785_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
